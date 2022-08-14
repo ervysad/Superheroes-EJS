a395ab5eb61c47078439c94e832d5e41c2f3d806
Brian Camilo Valencia <bcvalenciap@unal.edu.co>
2022-08-14 18:11:21 -0500

    awdw

--- index.js
+++ index.js
@@ -2,235 +2,87 @@ const express = require("express");
 const parser = require("body-parser");
 const app = express();
 app.set('view engine', 'ejs');
-app.use("/public",express.static("public")); //this metod let the public folder to be static in order to read the complement files.
+app.use("/public", express.static("public")); //this metod let the public folder to be static in order to read the complement files.
 
 const https = require("https");
-var baseUrl = "https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes";
-
-var names = [];
-var imgsrcs = [];
-var occupation = [];
-var alignment = [];
-var location = [];
+var baseUrl = "https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes?page=";
 
 
-app.get("/",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=1";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
+var pageObjs = [];
+
+
+
+https.get(baseUrl + 1, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[0]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 2, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[1]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 3, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[2]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 4, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[3]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 5, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[4]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 6, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[5]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 7, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[6]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 8, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[7]= JSON.parse(urldata);
+    })
+})
+https.get(baseUrl + 9, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[8]= JSON.parse(urldata);
+    })
+})
+
+https.get(baseUrl + 10, function (urlresponse) {
+    urlresponse.on("data", function (urldata) {
+        pageObjs[9]= JSON.parse(urldata);
+    })
+})
+
+
+
+
+app.get("/", function (req, res) {
+    res.render("index", {pageObjejs:pageObjs[0]})
 });
 
-
-app.get("/page2",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=2";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page3",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=3";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page4",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=4";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-
-app.get("/page5",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=5";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-
-app.get("/page6",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=6";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page7",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=7";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page8",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=8";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page9",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=9";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
-app.get("/page10",function(req, res){
-    baseUrl="https://ea1w717ym2.execute-api.us-east-1.amazonaws.com/api/heroes/?page=10";
-    //get all superheroes info 
-    https.get(baseUrl, function (urlresponse) {  
-        console.log(urlresponse.statusCode); 
-        urlresponse.on("data",function(urldata){   
-            var arraydata = JSON.parse(urldata);
-            for (var i = 0; i < arraydata.items.length; i++) {
-                names[i] = arraydata.items[i].name;
-                imgsrcs[i] = arraydata.items[i].images.lg;
-                occupation[i] = arraydata.items[i].work.occupation;
-                alignment[i] = arraydata.items[i].biography.alignment;
-                location[i] = arraydata.items[i].work.base;
-                
-            }
-            console.log(names);
-        }); 
-    }); 
-    res.render("index",{namesejs: names, imagesejs:imgsrcs, occupationejs:occupation, alignmentejs:alignment, locationejs:location});
-});
-
+for (let i = 1; i < 11; i++) {
+    app.get("/page"+i, function (req, res) {
+        res.render("index", {pageObjejs:pageObjs[i-1]})
+        console.log(pageObjs[i-1].page);
+    });
+    
+}
 
 
 
 var port = 3000;
-app.listen(port , function(){
-    console.log("server started at port: "+port);
+app.listen(port, function () {
+    console.log("server started at port: " + port);
 });
+
